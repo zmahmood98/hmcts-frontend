1a92781d4e3155cfd7c9460c1cad6ba713146c8f
zahra <zahra.mahmood98@hotmail.com>
2025-09-19 20:19:19 +0100

    Add toolbar component

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,9 @@
+function Toolbar() {
+    return (
+        <div className="toolbar">
+            <h1 className="toolbar-title">Task Manager</h1>
+        </div>
+    );
+}
+
+export default Toolbar;
