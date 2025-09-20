c9ae7b22633dc08b106b1119708dac4703026d7d
zahra <zahra.mahmood98@hotmail.com>
2025-09-20 02:11:59 +0100

    Add due date to task cards

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,25 @@
+import { parseISO, format } from "date-fns";
+
 function TaskCard({ task }) {
-    return (
-        <div className="task-card">
-            <h4 className="task-card-title">{task.title}</h4>
-            {task.description && <h5 className="task-card-desc">{task.description}</h5>}
-        </div>
-    );
+  function handleClick() {}
+
+  function dateDisplay(isoDate) {
+    const date = parseISO(isoDate);
+    return format(date, "EEEE do MMM");
+  }
+
+  return (
+    <div className="task-card">
+      <button onClick={handleClick} className="task-more-button">
+        ...
+      </button>
+      <h4 className="task-card-title">{task.title}</h4>
+      {task.description && (
+        <h5 className="task-card-desc">{task.description}</h5>
+      )}
+      <h6 className="task-card-date">Due by: {dateDisplay(task.dueDate)}</h6>
+    </div>
+  );
 }
 
 export default TaskCard;
