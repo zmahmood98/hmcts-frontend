6fd74586f2095a39175fa374db3a219c0999952b
zahra <zahra.mahmood98@hotmail.com>
2025-09-20 01:28:18 +0100

    Fix indents

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,19 +1,19 @@
 import TaskColumn from "../components/TaskColumn";
 
 function Board() {
-    return (
-      <div className="board">
-        <div className="task-column tc-todo">
-          <TaskColumn status="To do" title="To Do" />
-        </div>
-        <div className="task-column tc-doing">
-          <TaskColumn status="Doing" title="Doing" />
-        </div>
-        <div className="task-column tc-done">
-          <TaskColumn status="Done" title="Done" />
-        </div>
+  return (
+    <div className="board">
+      <div className="task-column tc-todo">
+        <TaskColumn status="To do" title="To Do" />
       </div>
-    );
+      <div className="task-column tc-doing">
+        <TaskColumn status="Doing" title="Doing" />
+      </div>
+      <div className="task-column tc-done">
+        <TaskColumn status="Done" title="Done" />
+      </div>
+    </div>
+  );
 }
 
 export default Board;
